c0251c0c341c7ff7976f15290eeb07949bc8b142
Neeraj chaudhary <95501768+neechaudhary@users.noreply.github.com>
2023-05-01 16:21:53 +0530

    d

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const allowed_origins = [
     'http://localhost:3001',
     'http://localhost:3047',
     "http://127.0.0.1:5500",
-    "https://aashiq.vercel.app/",
+    "https://aashiq.vercel.app",
     "https://admin-jan-seva-kendra.vercel.app",
     "https://jan-seva-kendra.vercel.app",
     "https://react-jsk-harshaaweb.vercel.app",
